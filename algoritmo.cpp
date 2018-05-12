d3020df1170010ce5cf6c0e5eb50400a05e9033b
hugomatheuss <hugomatheus650@gmail.com>
2018-05-12 19:09:57 -0300

    v1.6

--- algoritmo.cpp
+++ algoritmo.cpp
@@ -5,20 +5,22 @@
 using namespace std;
 
 int main () {
-  vector< vector < int > > d_curso;         //vetor com as disciplinas (ids)
-  //vector< vector < int > > curso_ch;       //vetor com a carga horária de cada disciplina
-  int d_ch[24][2];
-  vector<int> random_schedule;
+  vector<int> d_t1, d_t2, d_t3, d_t4;         
+  vector<int> d_ch;
+  vector<int> h_t1, h_t2, h_t3, h_t4;
   int x = 0;
 
-  for(int i = 0; i < 4; i++) {
-    inicia_disciplinas(d_curso, x);
-    x++;                                    //contador de períodos por curso
-  }
+  inicia_disciplinas(d_t1, 0);
+  inicia_disciplinas(d_t2, 1);
+  inicia_disciplinas(d_t3, 2);
+  inicia_disciplinas(d_t4, 3);
 
   inicia_carga_horaria(d_ch);
 
-  random_fill(d_ch, random_schedule);
+  some_name(d_t1, d_ch, h_t1);
+  some_name(d_t2, d_ch, h_t2);
+  some_name(d_t3, d_ch, h_t3);
+  some_name(d_t4, d_ch, h_t4);
 
   return 0;
 }
--- burn.h
+++ burn.h
@@ -6,103 +6,52 @@
 
 using namespace std;
 
-//função que preenche o vetor de disciplinas
-void inicia_disciplinas(vector< vector < int > >& d_curso, int x){
-  int num, y = 0;
-  string line;
-  vector<int> v_aux;
+//função utilizada para gerar a solução inicial aleatória
+void random_fill(vector<int>& random_schedule) {
+  random_shuffle(random_schedule.begin(), random_schedule.end());
+  cout << endl;
+
+  cout << "Shuffle vector" << endl;
+  for(int i = 0; i < random_schedule.size(); i++) {
+    cout << random_schedule[i] << endl;
+  }
+}
+
+//função que gera solução inicial
+void inicia_disciplinas(vector<int>& d, int x){
+  int num = 0;
+  string line, path;
 
   cout << endl << "Iniciando processo..." << endl;
 
-  if(x == 0) {
-    ifstream c1_t1 ("c1t1.txt");
-    if(!c1_t1.is_open())
-      cout << "Não foi possível abrir o arquivo!" << endl;
-    else {
-      cout << "WHILE" << endl;
-      while(getline(c1_t1, line)) {
-        num = stoi(line);
-        v_aux.push_back(num);
-      }
-      d_curso.push_back(v_aux);
-      c1_t1.close();
-      cout << "Curso 1 - Turma 1" << endl;
-      for(int i = 0; i < d_curso.size(); i++) {
-        for(int j = 0; j < d_curso[i].size(); j++) {
-          cout << d_curso[i][j] << " ";
-        }
-        cout << endl;
-      }
+  if(x == 0) {path = "c1t1.txt";}
+  else if(x == 1) {path = "c1t2.txt";}
+  else if(x == 2) {path = "c1t3.txt";}
+  else {path = "c1t4.txt";}
+
+  ifstream d_file (path);
+  if(!d_file.is_open())
+    cout << "Não foi possível abrir o arquivo!" << endl;
+  else {
+    while(getline(d_file, line)) {
+      num = stoi(line);
+      d.push_back(num);
     }
+    d_file.close();
+    cout << "Curso 1 - Turma "<< x+1 << endl;
+    for(int i = 0; i < d.size(); i++) {
+      cout << d[i] << " ";
+    }
+    random_fill(d);
   }
-  else if(x == 1) {
-    ifstream c1_t2 ("c1t2.txt");
-    if(!c1_t2.is_open())
-      cout << "Não foi possível abrir o arquivo!" << endl;
-      else {
-        while(getline(c1_t2, line)) {
-          num = stoi(line);
-          v_aux.push_back(num);
-        }
-        d_curso.push_back(v_aux);
-        c1_t2.close();
-        cout << "Curso 1 - Turma 2" << endl;
-        for(int i = 0; i < d_curso.size(); i++) {
-            for(int j = 0; j < d_curso[i].size(); j++) {
-              cout << d_curso[i][j] << " ";
-            }
-            cout << endl;
-        }
-      }
-  }
-  else if(x == 2) {
-    ifstream c1_t3 ("c1t3.txt");
-    if(!c1_t3.is_open())
-      cout << "Não foi possível abrir o arquivo!" << endl;
-      else {
-        while(getline(c1_t3, line)) {
-          num = stoi(line);
-          v_aux.push_back(num);
-        }
-        d_curso.push_back(v_aux);
-        c1_t3.close();
-        cout << "Curso 1 - Turma 3" << endl;
-        for(int i = 0; i < d_curso.size(); i++) {
-            for(int j = 0; j < d_curso[i].size(); j++) {
-              cout << d_curso[i][j] << " ";
-            }
-            cout << endl;
-        }
-      }
-  }
-  else if(x == 3) {
-    ifstream c1_t4 ("c1t4.txt");
-    if(!c1_t4.is_open())
-      cout << "Não foi possível abrir o arquivo!" << endl;
-      else {
-        while(getline(c1_t4, line)) {
-          num = stoi(line);
-          v_aux.push_back(num);
-        }
-        d_curso.push_back(v_aux);
-        c1_t4.close();
-        cout << "Curso 1 - Turma 4" << endl;
-        for(int i = 0; i < d_curso.size(); i++) {
-          for(int j = 0; j < d_curso[i].size(); j++) {
-            cout << d_curso[i][j] << " ";
-          }
-          cout << endl;
-        }
-      }
-  }
+
   cout << endl << "disciplinas preenchidas" << endl;
 }
 
 //função que preenche o vetor de carga horária referente a cada disciplina
-void inicia_carga_horaria(int d_ch[24][2]){
+void inicia_carga_horaria(vector<int>& d_ch){
   string line;
   int num = 0;
-  int x = 0;
   vector<int> v_aux;
   ifstream in_file ("in.txt");
 
@@ -113,51 +62,39 @@ void inicia_carga_horaria(int d_ch[24][2]){
   }
   else {
     while(getline(in_file, line)) {
-      //x++;
-      //if(line == "80"){num = 80;v_aux.push_back(x);v_aux.push_back(num);} else if(line == "40") {num = 40;v_aux.push_back(x);v_aux.push_back(num);}
       if(line=="80") {num=80;} else if(line=="40") {num=40;}
-      v_aux.push_back(num);
+      d_ch.push_back(num);
     }
-    for(int i = 0; i < 24; i++) {
-      for(int j = 0; j < 2; j++) {
-        if(j == 0) {d_ch[i][j] = (i+1);}
-        else {
-           d_ch[i][j] = v_aux[i];
-        }
-      }
-    }
-    //curso_ch.push_back(v_aux);
   }
   in_file.close();
-  for(int i = 0; i < 24; i++) {
-    for(int j = 0; j < 2; j++) {
-      cout << d_ch[i][j] << " ";
-    }
-    cout << endl;
+  for(int i = 0; i < d_ch.size(); i++) {
+    cout << d_ch[i] << endl;;
   }
-  cout << endl << "Carga horária preenchida." << endl;
+  cout << endl << "Carga horária preenchida." << endl << endl;;
 }
 
-void random_fill(int d_ch[24][2], vector<int>& random_schedule) {
-  int x = 0;
-  vector<int> v_aux;
+void some_name(vector<int>& d, vector<int>& d_ch, vector<int>& h) {
+  vector<int> d_80;
+  int disc, aux, y = 0;
 
-  for(int i = 0; i < 24; i++) {
-    for(int j = 0; j < 2; j++) {
-      if(j == 0) {x = d_ch[i][j]; random_schedule.push_back(x);}
+  for(int i = 0; i < d.size(); i++) {
+    disc = d[i];
+    if(d_ch[disc-1] == 80) {
+      if((i+1) == d.size()) {h.push_back(disc); h.push_back(disc);}
+      else {d_80.push_back(disc); h.push_back(disc);}
+    }
+    else if(d_ch[disc-1] == 40) {
+      h.push_back(disc);
+      if((i+1) == d.size()) {
+        for(int i = y; i < d_80.size(); i++) {aux = d_80[i]; h.push_back(aux);}
+      }
+      else if(((y+1) <= d_80.size()) && !(d_80.empty())) {
+        aux = d_80[y];
+        h.push_back(aux);
+        y++;
+      }
     }
   }
-
-  cout << "Vetor inicial" << endl;
-  for(int i = 0; i < random_schedule.size(); i++) {
-    cout << random_schedule[i] << endl;
-  }
+  for(int i = 0; i < h.size(); i++) {cout << h[i] << " ";}
   cout << endl;
-
-  random_shuffle(random_schedule.begin(), random_schedule.end());
-
-  cout << "Shuffle vector" << endl;
-  for(int i = 0; i < random_schedule.size(); i++) {
-    cout << random_schedule[i] << endl;
-  }
 }
